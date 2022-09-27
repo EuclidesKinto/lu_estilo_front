da4d5b4a4564d19c8779879d25fd551b7bbbee4a
euclides <euclideskinto@gmail.com>
2022-09-27 14:52:17 -0300

    commit initial

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Header/>
+    <Nuxt/>
+  </div>
+</template>
+
+<script>
+import Header from "~/components/header/Header";
+export default {
+  name: "default",
+  components: {Header}
+}
+</script>
+
+<style scoped>
+
+</style>
